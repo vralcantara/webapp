c8b862819be347574dcc4c66c9653e93bc649ec3
Vitor Alcantara Barbosa <vralcantara.ti@gmail.com>
2020-06-03 15:53:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
-	  res.end('Super APP TOP da Maua Ver 2.0.0');
+	  res.end('Super APP de Exemplo Ver 1.0.0');
 }).listen(8080);
 console.log('Atendendo em: http://localhost:8080/');
